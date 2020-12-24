211fd045243a37886848a081b2169779a5529062
yangYlin <1850571794@qq.com>
2020-12-24 22:20:56 +0800

    chapter12  'send mail to reset password'

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))  # 获取当前.py文件的绝对路径
+load_dotenv(os.path.join(basedir, 'microblog.env'))
 
 
 class Config:
@@ -8,3 +11,13 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/flask_micor_blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    POST_PER_PAGE = 3
+
+    # 从env文件里面读
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # 客户端授权密码,不是密码，是授权码
